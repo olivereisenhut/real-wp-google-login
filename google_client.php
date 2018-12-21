1516db7e2215f05003e67faf49ab8cd6c4cbde1b
Oliver Eisenhut <oliver.eisenhut@namics.com>
2018-12-21 14:11:47 +0100

    Clarify TODO

--- google_client.php
+++ google_client.php
@@ -1,6 +1,8 @@
 <?php
 defined('ABSPATH') or die('No script kiddies please!');
-// TODO load this from somewhere else
+// TODO load this from somewhere else like this:
+//$client->setClientId('dsjfsl');
+//$client->setClientId('dkfsd');
 $config_file_path = plugin_dir_path(__FILE__) . 'client_secret.json';
 
 $client = new Google_Client();
@@ -9,8 +11,6 @@ $client->addScope(Google_Service_Plus::USERINFO_PROFILE);
 $client->addScope(Google_Service_Plus::USERINFO_EMAIL);
 $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . "/";
 $client->setRedirectUri($redirect_uri);
-//$client->setClientId('dsjfsl');
-//$client->setClientId('dkfsd');
 
 $authUrl = $client->createAuthUrl();
 $service = new Google_Service_Plus($client);
